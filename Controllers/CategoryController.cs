a94340b7dfde00facce10c94e64d8f48a3947b3c
mike <mike@pop-os.localdomain>
2022-03-05 18:09:23 -0500

    update

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+using bulkybook.Data;
+using bulkybook.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace bulkybook.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View(objCategoryList);
+    }
+}
