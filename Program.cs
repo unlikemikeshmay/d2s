8d28096adfde4e4689c8d7df65e37fbb6897f490
mike <mike@pop-os.localdomain>
2022-03-21 22:27:21 -0400

    linked repos

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-builder.Services.AddScoped<IPlayerRepository>();
+builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
