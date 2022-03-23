7febc993fd401d5254157fc56500f3cd84e1b620
mike <mike@pop-os.localdomain>
2022-03-23 15:30:33 -0400

    added fixes to post request formatting

--- Data/Repositories/PlayerRepository.cs
+++ Data/Repositories/PlayerRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Web;
 
 namespace bulkybook.Data
 {
@@ -27,6 +28,7 @@ namespace bulkybook.Data
             try
             {
                 _httpClient.DefaultRequestHeaders.Add("x-api-key",_config.apiKey.ToString());
+                 //_httpClient.DefaultRequestHeaders.Add("Content-Type","application/x-www-form-urlencoded");
                 HttpResponseMessage response = await _httpClient.GetAsync(combinedUrl);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
@@ -47,19 +49,53 @@ namespace bulkybook.Data
              _config.apiKey = Guid.Parse(_configuration["apiKey"].ToString());
              _config.clientID = int.Parse(_configuration["clientID"]);
              _config.rootUrl = _configuration["rootUrl"].ToString();
+
+                var x = $"{_config.clientID}:{_config.apiKey}";
+                var y = System.Text.Encoding.UTF8.GetBytes(x);
+                var encoded = System.Convert.ToBase64String(y);
+
+
+            //_httpClient.DefaultRequestHeaders.Add("Content-Type","application/x-www-form-urlencoded");
+            _httpClient.DefaultRequestHeaders.Add("X-API-Key",$"{_config.apiKey}");
+            _httpClient.DefaultRequestHeaders.Add("Authorization",$"Basic {encoded}");
+
             OAuthResponse oauthresponse = new OAuthResponse();
-            string url = "https://www.bungie.net/platform/app/oauth/token/";
+
+            //convert authorization to base64
+
+            string url = $"https://www.bungie.net/Platform/App/OAuth/Token/?grant_type=authorization_code&client_id={_config.clientID}&code={id}";
+            
+            var urlEncoded = HttpUtility.UrlEncode(url);
+
             var dict = new Dictionary<string,string>();
-            dict.Add("grant_type",$"authorization_code&code={id}");
+            dict.Add("grant_type","authorization_code");
             dict.Add("client_id",_config.clientID.ToString());
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+            dict.Add("code",$"id");
+
             
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post,url){Content = new FormUrlEncodedContent(dict)};
+            
             
             HttpResponseMessage response = await _httpClient.SendAsync(request);
             var res =  await response.Content.ReadAsStringAsync();
             oauthresponse.membership_id =  res.ToString();
             return oauthresponse;
+
+
+
+            /* request.post({
+    url: 'https://www.bungie.net/Platform/App/OAuth/Token/',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + X
+    }, 
+    data: {
+      'grant_type': 'authorization_code',
+      'code': '7f39897b4a1b17e4f5753a0a90c21***'
+    }
+  }, function(err, res, body){
+    console.log(body);
+}); */
         }
     }
 }
