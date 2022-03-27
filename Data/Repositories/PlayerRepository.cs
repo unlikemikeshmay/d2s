432aa94a72afc74667e0173b06612eaf1224d10a
mike <mike@pop-os.localdomain>
2022-03-27 13:13:37 -0400

    update

--- Data/Repositories/PlayerRepository.cs
+++ Data/Repositories/PlayerRepository.cs
@@ -26,7 +26,7 @@ namespace bulkybook.Data
             {
             //might have to set bearer token- dunno yet
               Player player = new Player();
-              dynamic d = await url.WithOAuthBearerToken(bt).WithHeader("x-api-key",$"{_config.apiKey}").GetJsonAsync();
+              dynamic d = await url.WithHeader("x-api-key",$"{_config.apiKey}").WithHeader("Authorization", $"Bearer {bt}").GetJsonAsync();
                 player.membershipId = d.membershipId;
                 player.uniqueName = d.uniqueName;
                 player.normalizedName = d.normalizedName;
@@ -74,47 +74,6 @@ namespace bulkybook.Data
                 return player;
             }
         }
-       /*  public Task<OAuthResponse> AuthorizeUser(string id)
-        {
-             Config _config = new Config();
-             _config.apiKey = Guid.Parse(_configuration["apiKey"].ToString());
-             _config.clientID = int.Parse(_configuration["clientID"]);
-             _config.rootUrl = _configuration["rootUrl"].ToString();
-             _config.secret = _configuration["secret"];
-
-                var x = $"{_config.clientID}:{_config.secret}";
-                var y = System.Text.Encoding.UTF8.GetBytes(x);
-                var encoded = System.Convert.ToBase64String(y);
-
-
-            //_httpClient.DefaultRequestHeaders.Add("Content-Type","application/x-www-form-urlencoded");
-            _httpClient.DefaultRequestHeaders.Add("X-API-Key",$"{_config.apiKey}");
-            //_httpClient.DefaultRequestHeaders.Add("Authorization",$"Basic {x}");
-
-            OAuthResponse oauthresponse = new OAuthResponse();
-
-            //convert authorization to base64
-        //string url = $"https://www.bungie.net/Platform/App/OAuth/Token/?grant_type=authorization_code&client_id={_config.clientID}&code={id}";
-           
-            
-            
-
-            var dict = new Dictionary<string,string>();
-            dict.Add("grant_type","authorization_code");
-            dict.Add("code",$"id");
-            dict.Add("client_id",_config.clientID.ToString());
-            //dict.Add("redirect_uri","https://destinyscan.azurewebsites.net/home/loggedin/{code}");
-            
-
-            
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post,url){Content = new FormUrlEncodedContent(dict)};
-            
-            
-            HttpResponseMessage response =  _httpClient.Send(request);
-            //var res = response.Content.ReadAsStringAsync();
-            oauthresponse.membership_id =  response.ToString();
-            return oauthresponse;
-        } */
          public async Task<OAuthResponse> AuthorizeUser(string id)
         {
              Config _config = new Config();
