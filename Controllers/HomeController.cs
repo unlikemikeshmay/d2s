d10a01782dc1b91e582fb66545b8a913ebb5d83e
mike <mike@pop-os.localdomain>
2022-03-28 12:51:12 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -57,17 +57,18 @@ Config conf = new Config();
             OAuthResponse authToken = new OAuthResponse();
             pvm.Player = player;
             pvm.OAuthResponse = authToken;
+            ViewData["autht"] = pvm.OAuthResponse.access_token;
             // populate appropriate memories with player info
             //persistence needed? 
             ViewData["LayoutName"] = "_Layout";
             _logger.LogInformation("Session Token: {SeshToken}",code);
             ViewData["token"]= code;
-            authToken = await _playerRepository.AuthorizeUser(code);
+            //authToken = await _playerRepository.AuthorizeUser(code);
             
 
             //CHECK WHY ID DOESNT ACTUALLY EQUAL ID. RUN DEBUG AGAIN BESIDE LIVE
 
-            player = await _playerRepository.GetById(authToken.membership_id,authToken.access_token);
+            //player = await _playerRepository.GetById(authToken.membership_id,authToken.access_token);
             
 
             ViewData["authmem"] = pvm.OAuthResponse.access_token;
