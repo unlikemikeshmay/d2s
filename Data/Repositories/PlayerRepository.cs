75bb3975b186f219a0db963218807982bd1bdbaa
mike <michael.a.t.jay@gmail.com>
2022-05-20 08:34:54 -0400

    add inventory code argument to getprofile request

--- Data/Repositories/PlayerRepository.cs
+++ Data/Repositories/PlayerRepository.cs
@@ -140,7 +140,7 @@ namespace bulkybook.Data
              _config.clientID = int.Parse(_configuration["clientID"]);
              _config.rootUrl = _configuration["rootUrl"].ToString();
              _config.secret = _configuration["secret"];
-            string endpointUrl = $"/Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=100,200";
+            string endpointUrl = $"/Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=100,200,102";
             string completeUrl = $"{_config.rootUrl}{endpointUrl}";
             Console.WriteLine("Complete url: {0}",completeUrl);
              Player player = new Player();
