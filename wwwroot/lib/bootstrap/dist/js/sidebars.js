0eff30cbb4905be8c862bc0da0ae356530bdaa07
mike <michael.a.t.jay@gmail.com>
2022-06-21 21:08:49 -0400

    added attractive ui of sidebars - make the inventory work in those

---
+++ wwwroot/lib/bootstrap/dist/js/sidebars.js
@@ -0,0 +1,9 @@
+/* global bootstrap: false */
+(function () {
+  'use strict'
+  console.log("inside sidebar.js");
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
