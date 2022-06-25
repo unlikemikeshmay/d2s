c1f45450c2f72efb4ca09820e20f3c89bd67e842
mike <michael.a.t.jay@gmail.com>
2022-06-25 11:19:27 -0400

    chained promises instead of nesting them

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -5,18 +5,20 @@ import { GetProfileResponse } from "./Models/GetProfileResponse";
 import { Player } from "./Models/Player";
 import { DestinyCharacterComponent } from "./Models/DestinyCharacterComponent";
 import { CharactersViewModel } from "./Models/CharactersViewModel";
-
+var charactersViewModel: CharactersViewModel;
 const characterSorter = (character: DestinyCharacterComponent) => {
 
 }
-const startPopulating = (charArray: Array<GetDestinyCharacterResponse>) => {
-    if(charArray.length !== 3){
-        console.log(`globalCharacterArray len: ${charArray.length}`)
+const startPopulatingCharacters = (charactersViewModel: CharactersViewModel) => {
+    if(charactersViewModel.destinyCharacterResponse.length !== 3){
+        console.log(`globalCharacterArray len: ${charactersViewModel.destinyCharacterResponse.length}`)
     }else{
         console.log("did it");
-        console.log(charArray);
-        let username = document.getElementById("username");
-        username.innerHTML = 
+        console.log(charactersViewModel.destinyCharacterResponse);
+        console.log(charactersViewModel.destinyCharacterResponse.length);
+        console.log(`charactersViewModel.playerData.displayName: ${charactersViewModel.playerData.displayName}`);
+        document.getElementById("username").innerHTML = charactersViewModel.playerData.displayName;
+
         
     }
     
@@ -25,32 +27,35 @@ const startPopulating = (charArray: Array<GetDestinyCharacterResponse>) => {
 function IsNotNullOrUndefined<T>(object: T | undefined | null): object is T {
     return <T>object !== undefined && <T>object !== null;
 }
-const PopulateCharacterInventories = (getProfileResponse: GetProfileResponse,membership_type,membership_id) => {
+const PopulateCharacterInventories = (charactersViewModel: CharactersViewModel) => {
     console.log("PopulateCharacterInventories is called:");
     // i have profile and characters in this function:;:::
     try
     {
 
-    if(IsNotNullOrUndefined(getProfileResponse)){
+    if(IsNotNullOrUndefined(charactersViewModel.profileResponse)){
         //
         //assign ease of life variables
         let mem_id = "membership_id";
             let bt = "bearer";
             let id =  parseInt(GetCharacters.prototype.GetCookie(mem_id));
             let token = GetCharacters.prototype.GetCookie(bt);
-        var charArray = Array<GetDestinyCharacterResponse>();
+            var membership_id = charactersViewModel.userMembershipData.Response.destinyMemberships[0].membershipId;
+            var membership_type = charactersViewModel.userMembershipData.Response.destinyMemberships[0].membershipType;
+    
         // iterate through characterinventories map
         
         //get characterids
-        for (let key in getProfileResponse.Response.characterInventories.data){
+        for (let key in charactersViewModel.profileResponse.Response.characterInventories.data){
             let i = 0;
+           
             //globalCharacterIds.push(key);
                 GetCharacters.prototype.GetCharacter(parseInt(key),membership_id,membership_type,token)
                 .then(data => {
-                    charArray.push(data);
+                    charactersViewModel.destinyCharacterResponse.push(data);
                 })
                 .then(data => {
-                    startPopulating(charArray);
+                    startPopulatingCharacters(charactersViewModel);
                 })
                 .catch((error) => console.error("Error: ",error));
         }
@@ -60,7 +65,7 @@ const PopulateCharacterInventories = (getProfileResponse: GetProfileResponse,mem
 
         //build innerhtml list of inventory items based on character type in respective views
     }else{
-        console.error("Error: GetProfileResponse Passed to populatecharacterinventories is null or undefined:: ",getProfileResponse)
+        console.error("Error: GetProfileResponse Passed to populatecharacterinventories is null or undefined:: ",charactersViewModel.profileResponse)
     }
 }catch{
     //todo: add appropriate error message:
@@ -81,19 +86,62 @@ const PopulateCharacterInventories = (getProfileResponse: GetProfileResponse,mem
             let id =  parseInt(GetCharacters.prototype.GetCookie(mem_id));
             let token = GetCharacters.prototype.GetCookie(bt);
             let player: Player;
-            let charactersViewModel: CharactersViewModel;
+            
+            
             GetCharacters.prototype.GetBungieNetUserById(id,token)
-            .then(
+            .then(player => {
+                    //i believe the -1 tag is for all platforms so you can call getmembershipdatabyid,
+                    //this is relevant because you need getmebershipdatabyid to get destinymembershipid
+                    //to call get profile you need destinymembership id but the response contains membershiptype- which was needed in the prior request???
+                    var charactersViewModel: CharactersViewModel;
+                    charactersViewModel.playerData = player;
+                    console.log(`info in first getbungienetuserbyid: ${charactersViewModel}`);
+                    return GetCharacters.prototype.GetMembershipDataById(player.membershipId,-1,token);
+                })
+
+            .then(data => {
+                    console.log(`data in getmembershipbyid: ${data.Response.destinyMemberships[0].membershipType}`);
+                    //set these as vars to passthem into the populate character inventories so i can use them
+                    //down the line when access is lost
+                    var charactersViewModel: CharactersViewModel;
+                    charactersViewModel.userMembershipData = data;
+                    return charactersViewModel;
+                    })
+            .then(data => {
+                    
+                    GetCharacters.prototype.GetProfile(data.userMembershipData.Response.destinyMemberships[0].membershipId,data.userMembershipData.Response.destinyMemberships[0].membershipType,token)
+                    .then(data => {
+                        var charactersViewModel: CharactersViewModel;
+                        return charactersViewModel.profileResponse = data;
+                    })
+                    .catch(error => console.log(`Error: ${error}`));
+                    console.log(`charactersViewModel.profileResonse after it was assigned in .this() ${charactersViewModel.profileResponse}`);
+                    return charactersViewModel;
+                })
+            .then(data => {
+                console.log(`charactersViewModel.profileResonse after it was assigned in .this()`);
+                console.log(`charactersViewModel before it is sent to populatecharacterinventories: ${charactersViewModel}`);
+                    PopulateCharacterInventories(data);
+                    /* console.log(`data test: ${data.Response.characterInventories.data}`); */
+                            
+                    })
+            .catch((error) => console.error("Error: ",error));
+    }
+
+/*             .then(
                 player => {
                     //i believe the -1 tag is for all platforms so you can call getmembershipdatabyid,
                     //this is relevant because you need getmebershipdatabyid to get destinymembershipid
                     //to call get profile you need destinymembership id but the response contains membershiptype- which was needed in the prior request???
+                    
                     charactersViewModel.playerData = player;
+                    console.log(`info in first getbungienetuserbyid: ${charactersViewModel}`);
                     GetCharacters.prototype.GetMembershipDataById(player.membershipId,-1,token)
                     .then(data => {
                         console.log(`data in getmembershipbyid: ${data.Response.destinyMemberships[0].membershipType}`);
                         //set these as vars to passthem into the populate character inventories so i can use them
                         //down the line when access is lost
+                       
                         charactersViewModel.userMembershipData = data;
                         var membershipId = data.Response.destinyMemberships[0].membershipId;
                         var membershipType = data.Response.destinyMemberships[0].membershipType;
@@ -103,13 +151,4 @@ const PopulateCharacterInventories = (getProfileResponse: GetProfileResponse,mem
                                 charactersViewModel.profileResponse = data;
                                 PopulateCharacterInventories(charactersViewModel);
                                 /* console.log(`data test: ${data.Response.characterInventories.data}`); */
-                            }
-                        )
-                    })
-
-                }
-            )
-            
-            .catch((error) => console.error("Error: ",error));
-    }
-
+ 
