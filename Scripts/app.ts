2dbb0c49469c772ed8d0dee1eee94f9bf9e2c096
mike <michael.a.t.jay@gmail.com>
2022-07-31 19:32:42 -0400

    did it

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -5,12 +5,18 @@ import { GetProfileResponse } from "./Models/GetProfileResponse";
 import { Player } from "./Models/Player";
 import { DestinyCharacterComponent } from "./Models/DestinyCharacterComponent";
 import { CharactersViewModel } from "./Models/CharactersViewModel";
-var charactersViewModel: CharactersViewModel;
+import { GetUserMembershipData } from "./Models/GetUserMembershipData";
+var player = {} as Player;
+var charactersViewModel = {} as CharactersViewModel;
+const PROFILE_RESPONSE = "PROFILERESPONSE";
+const GET_MEMBERSHIP_DATA = "GETMEMBERSHIPDATA";
+const PLAYER = "PLAYER";
+
 const characterSorter = (character: DestinyCharacterComponent) => {
 
 }
-const startPopulatingCharacters = (charactersViewModel: CharactersViewModel) => {
-    if(charactersViewModel.destinyCharacterResponse.length !== 3){
+const startPopulatingCharacters = () => {
+    if(charactersViewModel.destinyCharacterResponse){
         console.log(`globalCharacterArray len: ${charactersViewModel.destinyCharacterResponse.length}`)
     }else{
         console.log("did it");
@@ -24,9 +30,53 @@ const startPopulatingCharacters = (charactersViewModel: CharactersViewModel) => 
 function IsNotNullOrUndefined<T>(object: T | undefined | null): object is T {
     return <T>object !== undefined && <T>object !== null;
 }
+
+function AssignGlobalVariable<T>(obj: T,trigger: string,charactersViewModel: CharactersViewModel) {
+    switch(trigger){
+        case PLAYER: 
+            player.membershipId = obj["membershipId"];
+            player.uniqueName = obj["uniqueName"];
+            player.normalizedName = obj["normalizedName"];
+            player.displayName = obj["displayName"];
+            player.profilePicture = obj["profilePicture"];
+            player.profileTheme = obj["profileTheme"];
+            player.userTitle = obj["userTitle"];
+            player.successMessageFlags = obj["successMessageFlags"];
+            player.isDeleted = obj["isDeleted"];
+            player.about = obj["about"];
+            player.firstAccess = obj["firstAccess"];
+            player.lastUpdate = obj["lastUpdate"];
+            player.legacyPortalUID = obj["legacyPortalUID"];
+            player.context = obj["context"];
+            player.psnDisplayName = obj["psnDisplayName"];
+            player.xboxDisplayName = obj["xboxDisplayName"];
+            player.fbDisplayName = obj["fbDisplayName"];
+            player.showActivity = obj["showActivity"];
+            player.locale = obj["locale"];
+            player.localeInheritDefault = obj["localeInheritDefault"];
+            player.lastBanReportId = obj["lastBanReportId"];
+            player.showGroupMessaging = obj["showGroupMessaging"];
+            player.profilePicturePath = obj["profilePicturePath"];
+            player.profileThemeName = obj["profileThemeName"];
+            player.userTitleDisplay = obj["userTitleDisplay"];
+            player.statusText = obj["statusText"];
+            player.statusDate = obj["statusDate"];
+            player.profileBanExpire = obj["profileBanExpire"];
+            player.blizzardDisplayName = obj["blizzardDisplayName"];
+            player.steamDisplayName = obj["steamDisplayName"];
+            player.stadiaDisplayName = obj["stadiaDisplayName"];
+            player.twitchDisplayName = obj["twitchDisplayName"];
+            player.cachedBungieGlobalDisplayName = obj["cachedBungieGlobalDisplayName"];
+            player.cachedBungieGlobalDisplayNameCode = obj["cachedBungieGlobalDisplayNameCode"];
+            break;
+        default:
+            console.log("default");
+    }
+    console.log("after switch: ",player);
+}
 const PopulateCharacterInventories = (charactersViewModel: CharactersViewModel) => {
     console.log("& * & * & * PopulateCharacterInventories is called:");
-    // i have profile and characters in this function:;:::
+    var charactersViewModel: CharactersViewModel;
     try
     {
 
@@ -50,6 +100,7 @@ const PopulateCharacterInventories = (charactersViewModel: CharactersViewModel) 
                 GetCharacters.prototype.GetCharacter(parseInt(key),membership_id,membership_type,token)
                 .then(data => {
                     charactersViewModel.destinyCharacterResponse.push(data);
+                    
                 })
                 .then(data => {
                     startPopulatingCharacters(charactersViewModel);
@@ -84,49 +135,64 @@ const PopulateCharacterInventories = (charactersViewModel: CharactersViewModel) 
             let ParsedId =  parseInt(GetCharacters.prototype.GetCookie(mem_id));
             let token = GetCharacters.prototype.GetCookie(bt);
             let player: Player;
-            let charactersViewModel: CharactersViewModel;
+            
             console.log(`id: ${id}`);
             console.log(`parseid: ${ParsedId}`);
+
+            //1
+
             GetCharacters.prototype.GetBungieNetUserById(ParsedId,token)
             .then(player => {
+                //AssignGlobalVariable(player,PLAYER,charactersViewModel);
                     //i believe the -1 tag is for all platforms so you can call getmembershipdatabyid,
                     //this is relevant because you need getmebershipdatabyid to get destinymembershipid
                     //to call get profile you need destinymembership id but the response contains membershiptype- which was needed in the prior request???
                     console.log(`player: ${player.displayName}`);
-                   
-                    
-                    return GetCharacters.prototype.GetMembershipDataById(player.membershipId,-1,token);
-                })
+                    //var memData: GetUserMembershipData;
+                    charactersViewModel.playerData = player;
 
-            .then(data => {
-                    console.log(`data: ${data.Response.bungieNetUser.displayName}`);
-                    console.log(`data in getmembershipbyid: ${data.Response.destinyMemberships[0].membershipType}`);
-                    //set these as vars to passthem into the populate character inventories so i can use them
-                    //down the line when access is lost
-                    let charactersViewModel: CharactersViewModel = {                    };
-                    charactersViewModel.userMembershipData = data;
-                    return charactersViewModel.userMembershipData = data;
+            //2  
+                    GetCharacters.prototype.GetMembershipDataById(player.membershipId,-1,token).then(data => {
+                      //  AssignGlobalVariable(data,GET_MEMBERSHIP_DATA,charactersViewModel);
+                      console.log("data in second then of getmembershiptdata",data);
+                        //memData = data;
+                        charactersViewModel.userMembershipData = data;
                     })
-            .then(data => {
-                var charactersViewModel: CharactersViewModel;
-                    GetCharacters.prototype.GetProfile(data.Response.destinyMemberships[0].membershipId,data.Response.destinyMemberships[0].membershipType,token)
-                    .then(data => {
-                        
-                        charactersViewModel.profileResponse = data;
-                        return charactersViewModel;
-                    })
-                    .catch(error => console.log(`Error: ${error}`));
-                    console.log(`charactersViewModel.profileResonse after it was assigned in .this() ${charactersViewModel.profileResponse}`);
+                    console.log("charactersviewmodel being returned from second then: ",charactersViewModel);
                     return charactersViewModel;
                 })
+
+            //3
             .then(data => {
-                console.log(`charactersViewModel.profileResonse after it was assigned in .this()`);
-                console.log(`charactersViewModel before it is sent to populatecharacterinventories: ${charactersViewModel.userMembershipData.Response.bungieNetUser.displayName}`);
-                    PopulateCharacterInventories(data);
-                    
-                            
+
+                    //var profileResponse: GetProfileResponse;
+                    console.log("data in third then after being passed veiwmodel with membershipdata: ",data)
+                    /* GetCharacters.prototype.GetProfile(data.Response.bungieNetUser..userMembershipData.Response.destinyMemberships[0].membershipId,data.userMembershipData.Response.destinyMemberships[0].membershipType,token) */
+                    GetCharacters.prototype.GetProfile(data.userMembershipData.Response.destinyMemberships[0].membershipId,data.userMembershipData.Response.destinyMemberships[0].membershipType,token)
+            //4
+                    .then(data => {
+                        console.log("data of getprofile response passed in fourth then: ",data);
+
+                       // AssignGlobalVariable(data,PROFILE_RESPONSE, charactersViewModel);
+                        //profileResponse = data;
+                        charactersViewModel.profileResponse = data;
+                        console.log(`charactersviewmodel after being passed getprofile response in fourth then ${charactersViewModel}`)
+                        return charactersViewModel;
+                        
                     })
-            .catch((error) => console.error("Error: ",error));
+                   
+                   
+                })
+                .then(data => {
+
+                    console.log(`charactersViewModel.profileResonse after it was assigned in fifth .this()${data}`);
+                    //console.log(`charactersViewModel before it is sent to populatecharacterinventories: ${charactersViewModel.userMembershipData.Response.bungieNetUser.displayName}`);
+                    startPopulatingCharacters(charactersViewModel);
+                        
+                                
+                        })
+                .catch((error) => console.error("Error in PopulateCharacterInventories: ",error)); 
+
     }
 
 
